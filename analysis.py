90e3a9cd7968c11ef59fcb7e0e1872daae32c8ba
James Slopey <181002700+j-slopey@users.noreply.github.com>
2025-04-23 20:08:54 -0600

    axis labels

--- analysis.py
+++ analysis.py
@@ -17,20 +17,23 @@ connection = pg8000.connect(
 cursor = connection.cursor()
 
 print("Retrieving data from database...")
+# Only analyzing bottom 99% of raters by frequency
 cursor.execute("""
                SELECT COUNT(rating) AS num_ratings, mean_score
                FROM user_info JOIN user_ratings USING(user_id)
                WHERE mean_score != 0
                GROUP BY user_id, mean_score
-               HAVING COUNT(rating) < 5000;
-               
+               HAVING COUNT(rating) < 1804;
                """)
 data = cursor.fetchall()
 
 print("Building scatterplot...")
 num_ratings = [pair[0] for pair in data]
 mean_score = [pair[1] for pair in data]
-plt.scatter(num_ratings, mean_score)
+plt.scatter(num_ratings, mean_score, marker='x', s=10)
+plt.xlabel("Total Number of Ratings By User")
+plt.ylabel("Average Score Given")
+plt.title("Average Score vs. Number of Ratings")
 plt.show();
 
 

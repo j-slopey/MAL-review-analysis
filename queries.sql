d6d5bc693e09ba0d6e242bf0720199bdcaff9640
James Slopey <181002700+j-slopey@users.noreply.github.com>
2025-04-23 20:24:00 -0600

    Created query to see score trends based on # of ratings given

---
+++ queries.sql
@@ -0,0 +1,32 @@
+-- Average rating given by each user grouped by the number of ratings they have given
+WITH rating_stats AS (
+    SELECT COUNT(rating) AS num_ratings, mean_score
+    FROM user_info JOIN user_ratings USING(user_id)
+    WHERE mean_score != 0
+    GROUP BY user_id, mean_score
+),
+percentiles AS (
+    SELECT 
+    (percentile_cont(0.25) WITHIN GROUP (ORDER BY num_ratings)) AS p_25,
+    (percentile_cont(0.5) WITHIN GROUP (ORDER BY num_ratings)) AS p_50,
+    (percentile_cont(0.75) WITHIN GROUP (ORDER BY num_ratings)) AS p_75,
+    (percentile_cont(0.99) WITHIN GROUP (ORDER BY num_ratings)) AS p_99
+    FROM rating_stats
+),
+filtered AS (
+    SELECT * FROM rating_stats WHERE num_ratings <= (SELECT p_99 FROM percentiles)
+),
+grouped AS (
+    SELECT num_ratings, mean_score,
+    CASE
+        WHEN num_ratings > 0 AND num_ratings <= (SELECT p_25 FROM percentiles) THEN '< 25th percentile'
+        WHEN num_ratings > (SELECT p_25 FROM percentiles) AND num_ratings <= (SELECT p_50 FROM percentiles) THEN '25th to 50th percentile'
+        WHEN num_ratings > (SELECT p_50 FROM percentiles) AND num_ratings <= (SELECT p_75 FROM percentiles) THEN '50th to 75th percentile'
+        WHEN num_ratings > (SELECT p_75 FROM percentiles) AND num_ratings <= (SELECT p_99 FROM percentiles) THEN '75th to 99th percentile'
+        ELSE 'Invalid Count'
+    END AS quartile
+    FROM filtered
+)
+SELECT quartile, AVG(mean_score) AS avg_rating 
+FROM grouped GROUP BY quartile ORDER BY quartile; 
+

9875b00044179121a01f3109776ce2d681dcbe43
dmills04 <118483949+dmills04@users.noreply.github.com>
2025-04-30 18:38:22 -0600

    Updated location analysis to deal with zeros and unknowns, and only shows top 3 for each country

--- location_analysis.sql
+++ location_analysis.sql
@@ -1,17 +1,23 @@
 SELECT  cleaned_location AS country,
-	COUNT(*) AS accounts,
-	ROUND(AVG(mean_score)::numeric, 2) AS avg_score,
-	ROUND(AVG(days_watched)::numeric, 2) AS avg_days_watched,
-	ROUND(AVG(total_entries)::numeric, 2) AS avg_total_entries
+        COUNT(*) AS accounts,
+        ROUND(AVG(mean_score)::numeric, 2) AS avg_score,
+        ROUND(AVG(days_watched)::numeric, 2) AS avg_days_watched,
+        ROUND(AVG(total_entries)::numeric, 2) AS avg_total_entries
 FROM cleaned_locations_trimmed
 GROUP BY cleaned_location;
 
+WITH grouped_ratings AS (
 SELECT  CLT.cleaned_location as country,
-	AG.genre,
-	AVG(UR.rating) as avg_rating,
-	COUNT(CLT.user_id) as total_ratings
+        AG.genre,
+        ROUND(AVG(UR.rating)::numeric, 2) as avg_rating,
+        COUNT(CLT.user_id) as total_ratings
 FROM cleaned_locations_trimmed AS CLT
 INNER JOIN user_ratings AS UR ON CLT.user_id = UR.user_id
 INNER JOIN anime_genres AS AG ON UR.anime_id = AG.anime_id
+WHERE AG.genre <> 'Unknown' AND UR.rating > 0
 GROUP BY CLT.cleaned_location, AG.genre
-HAVING COUNT(CLT.user_id) > 10;
+HAVING COUNT(CLT.user_id) > 10
+)
+SELECT country, genre, avg_rating, total_ratings FROM
+( SELECT *, ROW_NUMBER() OVER (PARTITION BY country ORDER BY avg_rating DESC) AS rn FROM grouped_ratings ) ranked
+WHERE rn <= 3; 
